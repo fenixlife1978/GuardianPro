a672bf82c303c6a8046c668c13bd2403461f7330
edwin aguiar <vallecondo@gmail.com>
2026-02-08 16:27:36 +0000

    si te sirve de algo estos datos usalos y no los dejes a desprotegidos y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7637044995-2342d",
-  "appId": "1:646726561371:web:1772f48489677d3891997e",
-  "apiKey": "AIzaSyD9aRR1-XCqv94dcliuhxg1mdAMMADIiZs",
-  "authDomain": "studio-7637044995-2342d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "646726561371"
+  apiKey: "AIzaSyD9aRR1-XCqv94dcliuhxg1mdAMMADIiZs",
+  authDomain: "studio-7637044995-2342d.firebaseapp.com",
+  projectId: "studio-7637044995-2342d",
+  storageBucket: "studio-7637044995-2342d.appspot.com",
+  messagingSenderId: "646726561371",
+  appId: "1:646726561371:web:1772f48489677d3891997e"
 };
